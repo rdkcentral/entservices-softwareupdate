6332a059c7792a196fd086033682f817b7928701
agampa263 <akshaykumar_gampa@comcast.com>
2025-04-04 00:09:24 +0000

    RDK-55373:[RDKServices] Coverity integration with middleware component workflow
    Reason for change: Coverity integration with middleware component workflow
    Test Procedure: Verify checks in github
    Risks: Low
    Signed-off-by:AkshayKumar_Gampa <AkshayKumar_Gampa@comcast.com>

--- cov_build.sh
+++ cov_build.sh
@@ -32,7 +32,7 @@ git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch main https://github.com/rdkcentral/entservices-apis.git
 
-git clone https://github.com/rdkcentral/entservices-casting.git
+git clone https://github.com/rdkcentral/entservices-softwareupdate.git
 
 git clone https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
 
@@ -168,11 +168,11 @@ cd ../../
 cp -r /usr/include/gstreamer-1.0/gst /usr/include/glib-2.0/* /usr/lib/x86_64-linux-gnu/glib-2.0/include/* /usr/local/include/trower-base64/base64.h .
 
 ############################
-# Build entservices-deviceanddisplay
+# Build entservices-softwareupdate
 echo "======================================================================================"
-echo "buliding entservices-deviceanddisplay"
+echo "buliding entservices-softwareupdate"
 cd $GITHUB_WORKSPACE
-cmake -G Ninja -S entservices-casting -B build/entservices-casting \
+cmake -G Ninja -S entservices-softwareupdate -B build/entservices-softwareupdate \
   -DUSE_THUNDER_R4=ON \
   -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
   -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
@@ -210,10 +210,9 @@ cmake -G Ninja -S entservices-casting -B build/entservices-casting \
   -DRDK_SERVICES_COVERITY=ON \
   -DRDK_SERVICES_L1_TEST=ON \
   -DDS_FOUND=ON \
-  -DPLUGIN_XCAST=ON \
-  -DPLUGIN_MIRACAST=ON
+  -DPLUGIN_FIRMWAREUPDATE=ON \
 
 
-cmake --build build/entservices-casting --target install
+cmake --build build/entservices-softwareupdate --target install
 echo "======================================================================================"
 exit 0
