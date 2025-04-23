492e94f44106b10b5dacfac65385ec952abedaf5
agampa263 <133222558+agampa263@users.noreply.github.com>
2025-04-23 15:25:40 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 ##############################
-GITHUB_WORKSPACE="${PWD}"
+#GITHUB_WORKSPACE="${PWD}"
 ############################
 # Build entservices-softwareupdate
 echo "======================================================================================"
 echo "buliding entservices-softwareupdate"
-cd $GITHUB_WORKSPACE
+#cd $GITHUB_WORKSPACE
 cmake -G Ninja -S $GITHUB_WORKSPACE/entservices-softwareupdate -B build/entservices-softwareupdate \
   -DUSE_THUNDER_R4=ON \
   -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
