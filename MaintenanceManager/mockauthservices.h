1f9a9629e24003d1c8834e0806b2ccbd712c1acc
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-06-18 12:06:08 +0530

    Update mockauthservices.h

--- MaintenanceManager/mockauthservices.h
+++ MaintenanceManager/mockauthservices.h
@@ -71,4 +71,20 @@ public:
     MOCK_METHOD(void, AddRef, (), (const, override));
     MOCK_METHOD(WPEFramework::PluginHost::ISecurity*, Officer, (const std::string& token), (override));
 };
+
+class DispatcherMock: public WPEFramework::PluginHost::IDispatcher{
+ public:
+         virtual ~DispatcherMock() = default;
+         MOCK_METHOD(void, AddRef, (), (const, override));
+         MOCK_METHOD(uint32_t, Release, (), (const, override));
+         MOCK_METHOD(void*, QueryInterface, (const uint32_t interfaceNummer), (override));
+         MOCK_METHOD(void, Activate, (WPEFramework::PluginHost::IShell* service));
+         MOCK_METHOD(WPEFramework::Core::hresult, Subscribe,
+            (ICallback* callback, const std::string& event, const std::string& designator),
+            (override));
+         MOCK_METHOD(void, Deactivate, ());
+         MOCK_METHOD(WPEFramework::Core::ProxyType<WPEFramework::Core::JSONRPC::Message>, Invoke, (const string&, uint32_t, const WPEFramework::Core::JSONRPC::Message&), (override));
+         
+};
+
 #endif
