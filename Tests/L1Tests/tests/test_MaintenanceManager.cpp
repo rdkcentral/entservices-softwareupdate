34e60a33ac04a7cdb40f5bb4fff9b1e3b1a7d78c
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-06-18 09:55:15 +0530

    Update test_MaintenanceManager.cpp

--- Tests/L1Tests/tests/test_MaintenanceManager.cpp
+++ Tests/L1Tests/tests/test_MaintenanceManager.cpp
@@ -1347,7 +1347,33 @@ TEST_F(MaintenanceManagerTest, SecManagerActive_AllGood_ReturnsTrue)
     EXPECT_FALSE(ok);
     //EXPECT_TRUE(g_subscribed_for_deviceContextUpdate);
 }
+TEST_F(MaintenanceManagerTest, SecManagerActive_AllGood_ReturnsTrue1)
+{
+   
+    //PluginHost::IShell::state state;
+    plugin_->m_service = &service_;
+/*
+   EXPECT_CALL(service_, QueryInterfaceByCallsign(::testing::_,"SecurityAgent"))
+	.Times(::testing::AtLeast(1))
+        .WillOnce(::testing::Return(&service_)); */
+   EXPECT_CALL(service_, QueryInterfaceByCallsign(::testing::_,"SecurityAgent"))
+   .Times(::testing::AtLeast(1))
+   .WillRepeatedly(Return(&service_));
 
+   EXPECT_CALL(service_, State())
+        .WillOnce(::testing::Return(PluginHost::IShell::state::ACTIVATED));
+
+    EXPECT_CALL(service_, QueryInterfaceByCallsign(::testing::_,"org.rdk.SecManager"))
+        .WillOnce(::testing::Return(&service_));
+    
+
+    //string activation = "not-activated";
+    std::string activation = "activated";
+    bool ok = plugin_->knowWhoAmI(activation);
+
+    EXPECT_FALSE(ok);
+    //EXPECT_TRUE(g_subscribed_for_deviceContextUpdate);
+}
 
 TEST_F(MaintenanceManagerTest, MaintenanceManagerOnBootup_InitializesCorrectly) {
     plugin_->m_service = &service_;
