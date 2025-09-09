e19d4ec51b56401939e780eac3cac0a4af6baaf0
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-09 15:35:28 +0530

    Update MaintenancemanagerL2.cpp

--- Tests/L2Tests/tests/MaintenancemanagerL2.cpp
+++ Tests/L2Tests/tests/MaintenancemanagerL2.cpp
@@ -6,8 +6,12 @@
 #include <iostream>
 #include <condition_variable>
 
-
+using ::testing::_;
+using ::testing::Invoke;
+using ::testing::Test;
+using ::testing::StrEq;
 using namespace WPEFramework;
+
 /*
 #define TEST_LOG(x, ...) fprintf( stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
