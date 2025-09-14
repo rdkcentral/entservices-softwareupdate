71914842f3a78251f0e457340642ef5b33eaa086
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-14 14:10:42 +0530

    Update MaintenancemanagerL2.cpp

--- Tests/L2Tests/tests/MaintenancemanagerL2.cpp
+++ Tests/L2Tests/tests/MaintenancemanagerL2.cpp
@@ -132,7 +132,7 @@ TEST_F(MaintenanceManagerTest, EmptyPartnerId) {
     EXPECT_NE(status, Core::ERROR_NONE); // Should fail or return error for empty partnerId
 }
 */
-//hjh
+
 /*
 TEST_F(MaintenanceManagerTest, ValidDeviceInitializationContext) {
 
