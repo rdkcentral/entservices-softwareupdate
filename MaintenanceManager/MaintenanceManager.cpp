d5535c301f4c93fa9feae0c27d867fcba25d5274
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-17 18:58:41 +0530

    Update MaintenanceManager.cpp

--- MaintenanceManager/MaintenanceManager.cpp
+++ MaintenanceManager/MaintenanceManager.cpp
@@ -2681,8 +2681,9 @@ namespace WPEFramework
 #endif
         }
         uint32_t MaintenanceManager::isConnectedToInternet(const JsonObject& parameters, JsonObject& response)
-        {
+        { 
            uint32_t rc = Core::ERROR_NONE;
+		   MM_LOGINFO("Inside isConnectedTointernet");
            response["connectedToInternet"] = "true";
            response["success"] = "true"; 
            return rc;           
