eb36397c845655e23b3124229d48c8142a9fd3ae
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-21 02:24:32 +0530

    Update FirmwareUpdate_L2Test.cpp

--- Tests/L2Tests/tests/FirmwareUpdate_L2Test.cpp
+++ Tests/L2Tests/tests/FirmwareUpdate_L2Test.cpp
@@ -67,7 +67,7 @@ FirmwareUpdateTest:: FirmwareUpdateTest():L2TestMocks()
          status = ActivateService("org.rdk.FirmwareUpdate");
          EXPECT_EQ(Core::ERROR_NONE, status);
          status = Core::ERROR_GENERAL;
-         status = ActivateService(org.rdk.NetworkManager.1);
+         status = ActivateService("org.rdk.NetworkManager.1");
          EXPECT_EQ(Core::ERROR_NONE, status);
          status = Core::ERROR_GENERAL;
          status = ActivateService("org.rdk.Network.1");
