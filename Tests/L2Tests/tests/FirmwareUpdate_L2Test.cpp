fc6a7fbcc287213d9abae0a4de4d63d92c7d7528
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-21 14:18:39 +0530

    Update FirmwareUpdate_L2Test.cpp

--- Tests/L2Tests/tests/FirmwareUpdate_L2Test.cpp
+++ Tests/L2Tests/tests/FirmwareUpdate_L2Test.cpp
@@ -16,7 +16,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "L2Tests.h"
