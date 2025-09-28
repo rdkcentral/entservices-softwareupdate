fbc82a67093195b2bf5153967812248774ed84db
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-28 13:29:57 +0530

    Update MaintenancemanagerL2.cpp

--- Tests/L2Tests/tests/MaintenancemanagerL2.cpp
+++ Tests/L2Tests/tests/MaintenancemanagerL2.cpp
@@ -359,7 +359,7 @@ TEST_F(MaintenanceManagerTest,Test6)
     JsonObject results,results1;
     params["maintenanceMode"] = "BACKGROUND";
     params["optOut"] = "IGNORE_UPDATE";
-    sleep(10);
+    sleep(20);
     status = InvokeServiceMethod("org.rdk.MaintenanceManager","stopMaintenance",params1, results1);
     ASSERT_EQ(results1["success"].Boolean(), true);
     ASSERT_EQ(status, Core::ERROR_NONE);
